4ff20cebf0999f8789672671849afdedbd2e757a
lukmccall <lukasz.kosmaty@student.uj.edu.pl>
2020-05-13 19:58:57 +0200

    Fix cnn

--- cnn_model_keres.py
+++ cnn_model_keres.py
@@ -8,18 +8,22 @@ from keras.datasets import cifar10
 from keras import activations
 from keras.optimizers import Adadelta
 from keras import metrics
+from keras.callbacks import TensorBoard
+from keras.optimizers import Adam
 import random 
 import numpy as np
 import scipy
+import os
 class CNN(object):
     def __init__(self, network=None):
         self.model = Sequential()
         self.datagen = None
+        self.validgen = None
 
     def init_input_layer(self, X):
         def preprocessing_function(image):
             if bool(random.getrandbits(1)):
-                sigma = random.uniform(0., 5.0)
+                sigma = random.uniform(0., 3.0)
                 image = scipy.ndimage.filters.gaussian_filter(image, sigma)
             return image
 
@@ -27,16 +31,23 @@ class CNN(object):
             featurewise_center=True,
             horizontal_flip=True,
             rotation_range=25,
+            featurewise_std_normalization=True,
             preprocessing_function=preprocessing_function, 
         )
+		
+        self.validgen = ImageDataGenerator(
+            featurewise_center=True,
+            featurewise_std_normalization=True,
+        )
+
         self.datagen.fit(X)
+        self.validgen.fit(X)
         shape = (X.shape[1], X.shape[2], X.shape[3])
         self.model.add(InputLayer(input_shape=shape))
         pass
 
     def define_network(self, X):
         self.init_input_layer(X)
-        self.model.add(BatchNormalization())
         self.model.add(
             Conv2D(
                 32,
@@ -71,6 +82,7 @@ class CNN(object):
                 padding="same",
             )
         )
+        #self.model.add(BatchNormalization())
         self.model.add(MaxPooling2D(pool_size=(2, 2), name="mp2", padding="same"))
         self.model.add(Flatten())
         self.model.add(Dense(
@@ -90,17 +102,21 @@ class CNN(object):
 
         self.model.compile(
             loss="categorical_crossentropy",
-            optimizer=Adadelta(),
-            metrics=[metrics.Accuracy()])
+            optimizer = Adam(learning_rate=0.0001),
+            metrics=['accuracy'])
     
 
-    def train(self, X, Y, X_validate, Y_validate):
-        print(self.datagen.flow(X, Y, batch_size=96).y.shape)
+    def train(self, model_dir, X, Y, X_validate, Y_validate):
+        logdir = os.path.join(model_dir, "logs") 
+        tensorboard_callback = TensorBoard(log_dir=logdir, write_graph=True, write_images=True, histogram_freq=0)
         self.model.fit(
             self.datagen.flow(X, Y, batch_size=96),
-            epochs=70,
-            validation_data=(X_validate, Y_validate),
+            epochs=140,
+            #validation_data=(X_validate, Y_validate),
+			validation_data=self.validgen.flow(X_validate, Y_validate),
             shuffle=True,
-            verbose=1
-
+            verbose=0,
+            callbacks=[tensorboard_callback],
+			#learning_rate = 0.001
         )
+        return self.model
--- learn.py
+++ learn.py
@@ -10,7 +10,7 @@ from utils import TRAIN_DATA_FILE_NAME, TEST_DATA_FILE_NAME
 import tflearn
 
 DATA_PATH = os.path.join('data', 'model')
-MODEL_OUTPUT = os.path.join('data', 'model')
+MODEL_OUTPUT = os.path.join('data', 'model18')
 
 VERSION = "alpha0.0.1"
 CLASSIFIER_NAME = "{}-classifier.tf1".format(VERSION)
@@ -42,8 +42,9 @@ X_test, Y_text = get_data_from_file(
 
 cnn = CNN()
 cnn.define_network(X)
-cnn.train(X, Y, X_test, Y_text)
-
+model = cnn.train(MODEL_OUTPUT, X, Y, X_test, Y_text)
+model.save(os.path.join(MODEL_OUTPUT, CLASSIFIER_NAME))
+print("Network trained and saved as {}".format(CLASSIFIER_NAME))
 # convnet = CNNModel()
 # network = convnet.define_network(X)
 # model = tflearn.DNN(network,
