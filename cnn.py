43580c48cf2d19c4d762363c31f982fba81ab6a1
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2020-05-13 20:35:28 +0200

    Update to tf 2.2 & remove tflearn

--- cnn.py
+++ cnn.py
@@ -2,7 +2,15 @@ from tensorflow import keras
 from keras import Sequential
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import BatchNormalization
-from keras.layers import Conv2D, Flatten, MaxPooling2D, Dropout, BatchNormalization, Dense, InputLayer
+from keras.layers import (
+    Conv2D,
+    Flatten,
+    MaxPooling2D,
+    Dropout,
+    BatchNormalization,
+    Dense,
+    InputLayer,
+)
 from keras import regularizers
 from keras.datasets import cifar10
 from keras import activations
@@ -10,10 +18,12 @@ from keras.optimizers import Adadelta
 from keras import metrics
 from keras.callbacks import TensorBoard
 from keras.optimizers import Adam
-import random 
+import random
 import numpy as np
 import scipy
 import os
+
+
 class CNN(object):
     def __init__(self, network=None):
         self.model = Sequential()
@@ -23,7 +33,7 @@ class CNN(object):
     def init_input_layer(self, X):
         def preprocessing_function(image):
             if bool(random.getrandbits(1)):
-                sigma = random.uniform(0., 3.0)
+                sigma = random.uniform(0.0, 3.0)
                 image = scipy.ndimage.filters.gaussian_filter(image, sigma)
             return image
 
@@ -32,12 +42,11 @@ class CNN(object):
             horizontal_flip=True,
             rotation_range=25,
             featurewise_std_normalization=True,
-            preprocessing_function=preprocessing_function, 
+            preprocessing_function=preprocessing_function,
         )
-		
+
         self.validgen = ImageDataGenerator(
-            featurewise_center=True,
-            featurewise_std_normalization=True,
+            featurewise_center=True, featurewise_std_normalization=True,
         )
 
         self.datagen.fit(X)
@@ -54,7 +63,7 @@ class CNN(object):
                 kernel_size=(5, 5),
                 activation=activations.relu,
                 name="conv1",
-                kernel_regularizer=regularizers.l2(), 
+                kernel_regularizer=regularizers.l2(),
                 bias_regularizer=regularizers.l2(),
                 padding="same",
             )
@@ -66,7 +75,7 @@ class CNN(object):
                 kernel_size=(5, 5),
                 activation=activations.relu,
                 name="conv2",
-                kernel_regularizer=regularizers.l2(), 
+                kernel_regularizer=regularizers.l2(),
                 bias_regularizer=regularizers.l2(),
                 padding="same",
             )
@@ -77,46 +86,37 @@ class CNN(object):
                 kernel_size=(3, 3),
                 activation=activations.relu,
                 name="conv3",
-                kernel_regularizer=regularizers.l2(), 
+                kernel_regularizer=regularizers.l2(),
                 bias_regularizer=regularizers.l2(),
                 padding="same",
             )
         )
-        #self.model.add(BatchNormalization())
+        # self.model.add(BatchNormalization())
         self.model.add(MaxPooling2D(pool_size=(2, 2), name="mp2", padding="same"))
         self.model.add(Flatten())
-        self.model.add(Dense(
-            512,
-            activation=activations.relu,
-            name="dense1",
-        ))
-        self.model.add(Dropout(
-            0.5,
-            name="dp1"
-        ))
-        self.model.add(Dense(
-            2,
-            activation=activations.softmax,
-            name="dense2",
-        ))
+        self.model.add(Dense(512, activation=activations.relu, name="dense1",))
+        self.model.add(Dropout(0.5, name="dp1"))
+        self.model.add(Dense(2, activation=activations.softmax, name="dense2",))
 
         self.model.compile(
             loss="categorical_crossentropy",
-            optimizer = Adam(learning_rate=0.0001),
-            metrics=['accuracy'])
-    
+            optimizer=Adam(learning_rate=0.0001),
+            metrics=["accuracy"],
+        )
 
     def train(self, model_dir, X, Y, X_validate, Y_validate):
-        logdir = os.path.join(model_dir, "logs") 
-        tensorboard_callback = TensorBoard(log_dir=logdir, write_graph=True, write_images=True, histogram_freq=0)
+        logdir = os.path.join(model_dir, "logs")
+        tensorboard_callback = TensorBoard(
+            log_dir=logdir, write_graph=True, write_images=True, histogram_freq=0
+        )
         self.model.fit(
             self.datagen.flow(X, Y, batch_size=96),
             epochs=140,
-            #validation_data=(X_validate, Y_validate),
-			validation_data=self.validgen.flow(X_validate, Y_validate),
+            # validation_data=(X_validate, Y_validate),
+            validation_data=self.validgen.flow(X_validate, Y_validate),
             shuffle=True,
             verbose=0,
             callbacks=[tensorboard_callback],
-			#learning_rate = 0.001
+            # learning_rate = 0.001
         )
         return self.model
--- utils.py
+++ utils.py
@@ -5,17 +5,17 @@ TEST_DATA_FILE_NAME = "test.txt"
 
 
 def for_each_sample(path, fn):
-    '''
+    """
         fn(sample, patient, patientDir)
         - sample - sample file name
         - patient - patient id 
         - patientDir - path to patient folder
-    '''
-    for patient in filter(lambda x: not x.startswith('.'), os.listdir(path)):
+    """
+    for patient in filter(lambda x: not x.startswith("."), os.listdir(path)):
         patientDir = os.path.join(path, patient)
         for sample in os.listdir(patientDir):
             fn(sample, patient, patientDir)
 
 
 def isCancerSample(sampleName):
-    return int(sampleName.split('-')[1].split('.')[0]) == 1
+    return int(sampleName.split("-")[1].split(".")[0]) == 1
